f33f3d07fd146af64f8d939f89e6411770074259
Timur Logachov <timur.logachov@teqblaze.com>
2025-11-12 00:06:01 +0200

    add publish views upd

--- resources/views/sections/roles/page.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('whitepage.layouts.app')
-
-@section('content')
-    @include('whitepage.components.title')
-    @include('whitepage.components.form', ['section' => 'project'])
-@endsection
